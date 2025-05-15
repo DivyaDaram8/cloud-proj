652323270f229af81f468686bf54099eceae8394
DivyaDaram8 <divyadaram0416@gmail.com>
2025-05-15 20:11:54 +0530

    backdrop modification

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -2,32 +2,35 @@ import { useRef } from "react";
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Backdrop = () => {
-
   const shadows = useRef();
- 
+
   return (
     <AccumulativeShadows
       ref={shadows}
-      temporal={true} // smooth out the edges of the shadows over time
-      frames={60}
-      alphaTest={0.85} // Transparency of the shadows
-      scale={10}
+      temporal
+      frames={100}
+      alphaTest={0.9}
+      scale={15}
       rotation={[Math.PI / 2, 0, 0]}
-      position={[0, 0, -0.14]}
+      position={[0, 0, -0.2]}
+      color="#1a1a1a"
+      colorBlend={0.5}
     >
       <RandomizedLight
-        amount={4}
-        radius={9}
-        intensity={0.55}
-        ambient={0.25}
-        position={[5, 5, -10]}
+        amount={5}
+        radius={10}
+        intensity={0.6}
+        ambient={0.3}
+        position={[7, 6, -10]}
+        bias={0.001}
       />
       <RandomizedLight
         amount={4}
-        radius={5}
-        intensity={0.25}
-        ambient={0.55}
-        position={[-5, 5, -9]}
+        radius={7}
+        intensity={0.4}
+        ambient={0.4}
+        position={[-6, 6, -8]}
+        bias={0.001}
       />
     </AccumulativeShadows>
   );
